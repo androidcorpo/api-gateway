fa20f9d6e7f39b6398cd3650f604d7be514223f3
Mbekou Severin <sgmbekou@gmail.com>
2023-04-01 16:31:50 +0400

    add test class

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 
 The following config enables rate limiters and routes the traffic to backend instance which is a helloworld service.
 This proxy runs on `8080` port and helloworld on `8081` port.
-
+`you must hava a running instance of redis`
 To test, just run the standalone RunBatch 
--- test/RunBatch.java
+++ test/RunBatch.java
@@ -3,17 +3,24 @@ package test;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RunBatch {
+
   public void hintApiCall(int n) throws IOException {
     String resourceUrl = "http://localhost:8080/helloworld";
     URL url = new URL(resourceUrl);
     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
     conn.setRequestMethod("GET");
-    for (int i = 0; i <= n; i++)
-      conn.getInputStream();
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i <= n; i++) {
+      int responseCode = conn.getResponseCode();
+      map.put(i, responseCode);
+    }
+    System.out.println(map);
+    conn.disconnect();
   }
-
   public static void main(String[] args) throws Exception {
     new RunBatch().hintApiCall(100);
   }
